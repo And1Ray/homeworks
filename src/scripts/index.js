9b2771a0298377fa65c915cba37d60a873de7ea5
Andrey Petukhov <petyhov.a@oneway.su>
2019-06-11 16:17:07 +0300

    metrics

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -87,6 +87,9 @@ $(document).ready(() => {
   let int;
 
   $openBtnPopup.click(() => {
+
+    ym(51856894, 'reachGoal', 'button');
+
     clearInterval(int);
     $popup.addClass('popup_active');
     int = setTimeout(() => {
